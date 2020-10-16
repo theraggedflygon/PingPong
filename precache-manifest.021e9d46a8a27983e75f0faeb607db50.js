b2275b2536c92ac8416db147439d2ccea22ab981
theraggedflygon <68510342+theraggedflygon@users.noreply.github.com>
2020-10-16 12:42:45 -0500

    Updates

---
+++ precache-manifest.021e9d46a8a27983e75f0faeb607db50.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b87f97779e36fac060c0b1217779ec",
+    "url": "/pingpong/index.html"
+  },
+  {
+    "revision": "1fa3535805d1b671d7d4",
+    "url": "/pingpong/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "4d73d1cfdf8afc34e9ff",
+    "url": "/pingpong/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1fa3535805d1b671d7d4",
+    "url": "/pingpong/static/js/2.5f39ebac.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pingpong/static/js/2.5f39ebac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d73d1cfdf8afc34e9ff",
+    "url": "/pingpong/static/js/main.ec1518da.chunk.js"
+  },
+  {
+    "revision": "957a538bdc6075c20168",
+    "url": "/pingpong/static/js/runtime-main.f96b2c0f.js"
+  },
+  {
+    "revision": "7a46f8de18ce728fcebc4d68b8853688",
+    "url": "/pingpong/static/media/corgi.7a46f8de.jpg"
+  },
+  {
+    "revision": "46acbc12c5bead2eca8174f957fe77da",
+    "url": "/pingpong/static/media/kelso.46acbc12.jpeg"
+  }
+]);
