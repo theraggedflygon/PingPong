35009d614f68b28a12aa404be8a844094e1b4206
theraggedflygon <68510342+theraggedflygon@users.noreply.github.com>
2021-02-26 18:21:13 -0600

    tournament 2 updates

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,22 +1,32 @@
-import React, { Component } from 'react';
-import {Container, Row, Col} from "react-bootstrap";
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 class Home extends Component {
-    state = {  }
-    render() { 
-        return ( 
-        <React.Fragment>
-            <Container>
-                <Row>
-                    <Col>
-                        <h2 style={{color: "white"}}>Middlebrook 7th Floor Ping Pong Classic - October 16th-17th, 2020</h2>
-                        <iframe title='Bracket' src="https://challonge.com/middlebrook_seventhfloor_pingpong/module" width="100%" height="1000" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
-                    </Col>
-                </Row>
-            </Container>
-        </React.Fragment>
-            );
-    }
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <Container>
+          <Row>
+            <Col>
+              <h2 style={{ color: "white" }}>
+                Middlebrook 7th Floor Ping Pong Classic - October 16th-17th,
+                2020
+              </h2>
+              <iframe
+                src="https://challonge.com/mb7winter/module"
+                width="100%"
+                height="500"
+                frameborder="0"
+                scrolling="auto"
+                allowtransparency="true"
+              ></iframe>{" "}
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Home;
